6a39ff4050d81d7e59063915fe97fed0be703fff
alinadevkota <devkota.alina@gmail.com>
2024-12-12 04:02:11 +0000

    UI for uploading files

--- app.py
+++ app.py
@@ -3,20 +3,44 @@ from langchain.llms import Ollama
 from datetime import datetime
 import uuid
 from utils import *
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 messages = []
 
 
+UPLOAD_FOLDER = 'documents'
+ALLOWED_EXTENSIONS = {'pdf'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Ensure the upload folder exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/', methods=['GET', 'POST'])
 def chat():
     if request.method == 'POST':
-        user_input = request.form['instruction']
-        
+        user_input = request.form.get('instruction', '')
+        file = request.files.get('file')
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # user_input += f" (File uploaded: {filename})"  # Append file info to the user input
+
+            retriever = process_documents(folder_path, embedding_model)
+            print("***************************************************************")
+            print("embedding generation completed after file '{filename}' upload.")
+
+        print("================================", user_input)
         # Generate bot's response
         response = generate_bot_response(user_input, chain)
-        
+
         # Store the messages (user and bot)
         id1 = str(uuid.uuid4())
         messages.append({"id": id1, "sender": "user", "text": user_input, "time": get_current_time()})
@@ -26,7 +50,7 @@ def chat():
         # Check if the request is an AJAX request
         if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
             return jsonify(messages=messages)  # Return the updated messages as JSON
-        
+
     return render_template('index.html', messages=messages)
 
 if __name__ == '__main__':
